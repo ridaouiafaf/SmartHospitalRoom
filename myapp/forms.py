56c646c296f3ee140a92ea220a5cd2baa8e90366
doha <doha@gmail.com>
2024-06-06 17:13:50 +0100

    pp

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,10 +1,4 @@
 from django import forms
-from .models import Chambre
-
-class ChambreForm(forms.ModelForm):
-    class Meta:
-        model = Chambre
-        fields = ['numero', 'etage', 'temperature', 'humidite', 'qualite_air']
 
 from django import forms
 from .models import *

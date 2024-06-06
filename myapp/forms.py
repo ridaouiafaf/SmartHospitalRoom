0449b61d3903fb57e05f749e9749e1ac173aa736
LAABIDI <houd.laabidi@gmail.com>
2024-06-06 21:42:47 +0200

    affectation du patient

--- myapp/forms.py
+++ myapp/forms.py
@@ -12,8 +12,8 @@ from .models import *
 class ChambreForm(forms.ModelForm):
     class Meta:
         model = Chambre
-        fields = ['numero', 'etage', 'temperature', 'humidite', 'qualite_air']
+        fields = ['numero', 'etage']
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields = ['id', 'nom', 'prenom', 'date_entree', 'date_sortie']
+        fields = ['id', 'nom', 'prenom', 'date_entree', 'date_sortie', 'chambre']
